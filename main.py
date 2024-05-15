8cbe98679f22ee7b5d2a07c46ad704ee998adcab
ADT109119 <2.jerry32262686@gmail.com>
2024-05-15 15:35:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -19,4 +19,4 @@ app.include_router(llm_model.router)
 app.include_router(line_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
